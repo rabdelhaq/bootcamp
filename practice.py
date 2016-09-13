e8d3b4ea64bacfb16ceed3dacf374d79849f26e3
Reem Abdel-Haq <rabdelha@caltech.edu>
2016-09-13 11:28:27 -0700

    new practice file

---
+++ practice.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+xa_high= np.loadtxt('data/xa_high_food.csv', comments = '#')
+xa_low= np.loadtxt('data/xa_low_food.csv', comments = '#')
+
+def xa_to_diameter(xa):
+    """
+    Convert an array of cross-sectional area to diameters.
+    """
+
+    #compute diamater from area
+    # A=pi * d^2/4
+    diameter= np.sqrt(4*xa/np.pi)
+    return diameter
