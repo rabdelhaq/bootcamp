79ae874481f155f5a7cdc7504c6aff9ac57f7833
Reem Abdel-Haq <rabdelha@caltech.edu>
2016-09-12 19:06:15 -0700

    updated function to print if original sequence was upper or lower case

--- dnatorna.py
+++ dnatorna.py
@@ -2,6 +2,10 @@
 
 def rna(seq):
     """Convert a DNA sequence to RNA."""
+    if seq==seq.upper():
+        print ("this sequence is uppercase")
+    else:
+        print ("this sequence is lowercase") 
 
     #convert to uppercase
     seq=seq.upper()
